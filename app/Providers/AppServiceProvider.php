7b24607535b73f1adaee0ea91248d2fac20eae2f
Grover Sean Reyes <Zilex.Seryth@gmail.com>
2020-06-09 17:56:39 +0800

    Schema::defaultStringLength(191);

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use KgBot\LaravelLocalization\Facades\ExportLocalizations;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,8 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+    
+        Schema::defaultStringLength(191);
         $localizationData = ExportLocalizations::export()->toFlat();
 
         View::composer('layouts.app', function ($view) use ($localizationData) {  
