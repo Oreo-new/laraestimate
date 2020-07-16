c316e48165bc502203f1156a24d7e3b7f5c940c5
clint <clint.responso@copypaste.ph>
2020-07-16 19:53:48 +0800

    homepage update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,10 +30,13 @@
                 <a class="navbar-brand mr-4" href="{{ url('/') }}">
                     <img src="{{ asset('/images/kloner.png') }}" alt="Kloner estimate Logo Image" height="25px">
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                @guest
+                    
+                @else 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -55,7 +58,7 @@
                             </a>
                         </li>
                     </ul>
-
+                @endguest
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
